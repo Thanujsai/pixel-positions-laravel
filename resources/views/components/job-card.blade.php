350c4bf05133203b8a9b77309e09112cb5efa160
thanujsai <sboda@modeln.com>
2025-05-03 20:22:53 +0530

    job card

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,8 +1,8 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group transition-colors duration-300">
     <div class="self-start text-sm">Laracasts</div>
-    <div class="py-8 font-bold">
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>{{-- if we hover over the parent div then this h3 will also consider it as it has been hovered--}}
+        <p class="text-xs mt-4">Full Time - From $60,000</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
