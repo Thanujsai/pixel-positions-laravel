da056dadb1bba57cbcb219a9d3e3edc25a55f0b9
Thanujsai <thanuj.sai14301@gmail.com>
2025-05-04 22:10:36 +0530

    group by featured jobs to distinguish between featured jobs and normal recent jobs

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,14 +1,14 @@
 @props(['job'])
 <x-panel class="flex flex-col text-center">
-    <div class="self-start text-sm">Laracasts</div>
+    <div class="self-start text-sm">{{$job->employer->name}}</div>
     <div class="py-8">
-        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>{{-- if we hover over the parent div then this h3 will also consider it as it has been hovered--}}
-        <p class="text-xs mt-4">Full Time - From $60,000</p>
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">{{ $job->title }}</h3>{{-- if we hover over the parent div then this h3 will also consider it as it has been hovered--}}
+        <p class="text-xs mt-4">Full Time - {{ $job->salary }}</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
             @foreach ($job->tags as $tag)
-                <x-tag :$tag size="small">Backend</x-tag>
+                <x-tag :$tag size="small"/>
             @endforeach
         </div>
 
