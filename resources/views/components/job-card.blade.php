cc516ae2c38b32ac2962cc6d89dacec4034234b6
Thanujsai <thanuj.sai14301@gmail.com>
2025-05-04 17:54:03 +0530

    tweaking

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,4 +1,4 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group transition-colors duration-300">
+<x-panel class="flex flex-col text-center">
     <div class="self-start text-sm">Laracasts</div>
     <div class="py-8">
         <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>{{-- if we hover over the parent div then this h3 will also consider it as it has been hovered--}}
@@ -6,11 +6,11 @@
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
+            <x-tag size="small">Backend</x-tag>
+            <x-tag size="small">Frontend</x-tag>
+            <x-tag size="small">Manager</x-tag>
         </div>
 
         <x-employer-logo :width="42"/>
     </div>
-</div>
+</x-panel>
--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,1 +1,19 @@
-<a href="#" class="bg-white/10 px-2 py-1 rounded-xl text-[10px] font-bold hover:bg-white/25 transition-colors duration-300">{{ $slot }}</a>
+@props(['size' => 'base'])
+
+@php
+    $classes = "bg-white/10 rounded-xl font-bold hover:bg-white/25 transition-colors duration-300";
+
+    Log::info("size");
+    Log::info($size);
+    if($size==='base'){
+        Log::info("in base");
+        $classes .= " px-5 py-1 text-sm";// . operator is to concatenate 2 strings
+    }
+
+    if($size==='small'){
+        Log::info("in small");
+        $classes .= " px-3 py-1 text-xs";
+    }
+@endphp
+
+<a href="#" class="{{ $classes }}">{{ $slot }}</a>
